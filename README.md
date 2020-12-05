be7bb115807a65a9e59746235c7078d8f4624621
Yvon Manzi <yvonmanzi@gmail.com>
2020-12-05 18:00:14 +0200

    getting ready to publish to crates

---
+++ README.md
@@ -0,0 +1,5 @@
+Lightweight and easy-to-use rust library for fetching
+trending repositories and developers. Relies on
+[github-trending-api](https://github.com/huchenme/github-trending-api)
+which is in JavaScript, so gtrending aims to fill the gap
+for rust.
--- src/fetch.rs
+++ src/fetch.rs
@@ -1,65 +1,69 @@
 use reqwest;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
 use std::error::Error;
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Deserialize, PartialEq, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct Repository {
     pub author: String,
     pub name: String,
-    pub avatar: String,
     pub url: String,
     pub description: String,
     pub language: String,
-    //pub language_color: String,
     pub stars: String,
     pub forks: String,
-    //pub current_period_stars: String,
-    //pub built_by: Vec<Person>,
+    pub avatar: String,
+    pub current_period_stars: String,
+    #[serde(skip)]
+    pub built_by: Vec<Person>,
 }
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Deserialize, PartialEq, Debug)]
 pub struct Person {
     pub href: String,
     pub avatar: String,
     pub username: String,
 }
-#[derive(Serialize, Deserialize, PartialEq)]
+#[derive(Deserialize, PartialEq)]
 pub struct ProgrammingLanguage {
     pub id: String,
     pub name: String,
 }
-#[derive(Serialize, Deserialize, PartialEq)]
+#[derive(Deserialize, PartialEq)]
 pub struct SpokenLanguage {
     pub url_param: String,
     pub name: String,
 }
-#[derive(Serialize, Deserialize, PartialEq)]
+
+#[derive(Deserialize, PartialEq, Debug)]
 pub struct Developer {
     pub username: String,
     pub name: String,
+    #[serde(rename = "type")]
     pub ttype: String,
     pub url: String,
     pub avatar: String,
     pub repo: Repo,
 }
-#[derive(Serialize, Deserialize, PartialEq)]
+#[derive(Deserialize, PartialEq, Debug)]
 pub struct Repo {
-    pub name: String,
+    pub repo_name: String,
     pub description: String,
-    pub url: String,
 }
 
 const URL: &str = "https://hackertab.pupubird.com";
 
-// Fetch trending repositories on GitHub
-//
-//Parameters:
-//  language (str, optional):  Filtering by language, eg: python
-//   spoken_language_code (str, optional): The spoken language, eg: en for english
-//  since (str, optional): The time range, choose from: [daily, weekly, monthly]. Defaults to "daily"
-
-//Returns:
-//    A list of dicts containing information for the trending repositories found
+/// Fetch trending repositories on GitHub
+///
+/// # Arguments
+///
+/// * `language` - the programming language to filter by. eg: python
+/// * `spoken_language_code` - the spoken language, eg: en for english
+/// * `since` - one of the `["daily", "weekly", "monthly"]`. Defaults to "daily"
+///
+///  # Returns
+///
+///  * `Result<Vec<Repository>, Box<dyn Error>>` - the list of all trending repositories fetched or error if fetching fails.
 pub async fn fetch_repos(
     language: Option<&str>,
     spoken_lang_code: Option<&str>,
@@ -67,6 +71,7 @@ pub async fn fetch_repos(
 ) -> Result<Vec<Repository>, Box<dyn Error>> {
     let mut query = String::from("/repositories?");
     if let Some(language) = language {
+        // Check if the programming language is valid
         if check_language_validity(language).await? {
             query = format!("{}language={}", query, language);
         }
@@ -79,12 +84,12 @@ pub async fn fetch_repos(
             query = format!("{}&since={}", query, since);
         }
     } else {
-        // defaults to 'daily'
+        // since defaults to "daily"
         query = format!("{}&since=daily", query);
     }
     if let Some(spoken_lang_code) = spoken_lang_code {
+        // check if spoken language is valid
         if check_spoken_lang_code_validity(spoken_lang_code).await? {
-            //needs formatting
             query = format!("{}&spoken_lang_code={}", query, spoken_lang_code);
         }
     } else {
@@ -100,19 +105,21 @@ pub async fn fetch_repos(
     return Ok(res);
 }
 
-// Fetch trending devs on GitHub
-//
-//Paramters:
-//  * language
-// * since
-//
-// Returns
-// * a Result enum of list of devs or error.
+/// Fetch trending devs on GitHub
+///
+/// # Arguments
+///
+///  * `language` - the programming language to filter by. eg: python
+/// * `since` - one of the `["daily", "weekly", "monthly"]`. Defaults to "daily"
+///
+/// Returns
+///
+/// * `Result<Vec<Developer>, Box<dyn std::error::Error>>` - the list of trending developers or error if fetching fails.
 pub async fn fetch_developers(
     language: Option<&str>,
     since: Option<&str>,
 ) -> Result<Vec<Developer>, Box<dyn std::error::Error>> {
-    let mut query = format!("/developers?");
+    let mut query = String::from("/developers?");
     if let Some(language) = language {
         if check_language_validity(language).await? {
             // need formating before being pushed to the querry
@@ -123,27 +130,32 @@ pub async fn fetch_developers(
     }
 
     if let Some(since) = since {
+        // Check since validity
         if check_since_validity(&since)? {
-            query = format!("{}&since={}", query, since);
+            query = format!("{}&since{}", query, since);
         }
     } else {
-        query = format!("{}&since=daily", query);
+        query = format!("{}&sincedaily", query);
     }
     let full_url = format!("{}{}", URL, query);
+    println!("{}", full_url);
     let res = reqwest::get(&full_url)
         .await?
         .json::<Vec<Developer>>()
         .await?;
     Ok(res)
 }
-
-//Check if the language exists.
-// parameters:
-// langugage(str): The language, eg: python.
-//
-// Returns:
-// A boolena value. True for valid langugae, False otherwise.
+/// Check if the language exists among valid languages.
+///
+/// # Arguments
+///
+///  * `language` - the programming language. eg: python
+///
+/// # Returns
+///
+/// * A boolena value. True for valid langugae, False otherwise.
 async fn check_language_validity(language: &str) -> Result<bool, Box<dyn Error>> {
+    // TODO: insteaf of fetchign these, i could just cache them
     let languages: Vec<ProgrammingLanguage> = languages_list().await?;
     let language = language.to_ascii_lowercase();
 
@@ -154,14 +166,15 @@ async fn check_language_validity(language: &str) -> Result<bool, Box<dyn Error>>
     }
     return Ok(false);
 }
-
-// Check if the spoken language exits.
-//
-// Parameters:
-// lang_code(str): the spoken language code, eg: en for english
-//
-// Returns:
-// A boolean value. True for valid speoken langauge, False otherwise
+/// Check if the spoken language exists.
+///
+/// # Arguments
+///
+/// * `lang_code` - the spoken language code. eg: en for English
+///
+/// # Returns
+///
+/// * `Result<bool, Box<dyn Error>>` `Ok(true)` for valid spoken language, `Ok(false)` otherwise.
 async fn check_spoken_lang_code_validity(lang_code: &str) -> Result<bool, Box<dyn Error>> {
     let spoken_languages = spoken_languages_list().await?;
     let lang_code = lang_code.to_ascii_lowercase();
@@ -173,21 +186,24 @@ async fn check_spoken_lang_code_validity(lang_code: &str) -> Result<bool, Box<dy
     }
     Ok(false)
 }
-// Check if the time range value is correct
-//
-// Parameters
-// * since(&str): the time range
-// Returns
-// * a boolean alue. true for valid parameter, false otherwise
+/// Check if the time range value is correct
+///
+/// # Arguments
+///
+/// * `since` - the time range
+///
+/// # Returns
+///
+/// * `Result<bool, Box<dyn Error>>` - `Ok(true)` for valid `since`, `Ok(false)` otherwise
 fn check_since_validity(since: &str) -> Result<bool, Box<dyn Error>> {
     Ok(["daily", "weekly", "monthly"].contains(&since))
 }
-// Fetches programming languages from GitHub.
-//
-// Paramters
-// *
-//Returns
-// *
+/// Fetches programming languages from GitHub.
+///
+/// # Returns
+///
+/// * `Result<Vec<ProgrammingLanguage>, Box<dyn Error>>` - the list of programming languages
+/// or error if fetching fails
 async fn languages_list() -> Result<Vec<ProgrammingLanguage>, Box<dyn Error>> {
     let url = format!("{}/languages", URL);
     let response: Vec<ProgrammingLanguage> = reqwest::get(&url)
@@ -197,10 +213,13 @@ async fn languages_list() -> Result<Vec<ProgrammingLanguage>, Box<dyn Error>> {
 
     Ok(response)
 }
-// Fetch spoken languages from GitHub.
-//
-// Returns
-// * A Result enum of either vector of spoken languages when successful or error in case of failure
+/// Fetch spoken languages from GitHub.
+///
+/// # Returns
+///
+/// * `Result<Vec<SpokenLanguage>, Box<dyn Error>>` - a list of spoken languages,
+/// or error if fetching fails
+
 async fn spoken_languages_list() -> Result<Vec<SpokenLanguage>, Box<dyn Error>> {
     let url = format!("{}/spoken_languages?", URL);
     let response: Vec<SpokenLanguage> = reqwest::get(&url)
